e5df06d4a2d3955fc62c2e224678afa081fc5c14
khosrow48 <69617633+khosrow48@users.noreply.github.com>
2020-08-13 11:05:02 +0430

    Add files via upload

---
+++ mybot.php
@@ -0,0 +1,11 @@
+﻿    include("Telegram.php");
+
+    date_default_timezone_set("asia/tehran");
+
+    // Set the bot TOKEN
+
+    $bot_id = "1362637450:AAEgAW-WLV2q6CIjIlyLxPXlOFRdiXmpfxY";
+
+    // Instances the class
+
+    $telegram = new Telegram($bot_id);
